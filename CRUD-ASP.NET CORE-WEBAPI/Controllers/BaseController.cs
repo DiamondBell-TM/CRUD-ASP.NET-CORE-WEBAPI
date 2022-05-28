29868ec4c392c993a688dd0fbcfc1e2750beb788
tmkarthick <karthick1989kumar@gmail.com>
2022-05-28 19:00:54 +0530

    K-1 ImplementedLogin and Registration Web API

---
+++ CRUD-ASP.NET CORE-WEBAPI/Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_ASP.NET_CORE_WEBAPI.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class BaseController : Controller
+    {
+    }
+}
